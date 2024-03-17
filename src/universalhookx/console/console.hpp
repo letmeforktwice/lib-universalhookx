cec6db6f0e5c6ba7a328595f9ba02850be606c34
KJK <kody.j.king@gmail.com>
2024-03-17 03:02:20 -0700

    - removed dependencies, consumer will supply these - removed dllmain - removed solution and project files since this will be a source library

---
+++ src/universalhookx/console/console.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <cstdio>
+
+//#define DISABLE_LOGGING_CONSOLE
+
+#ifndef DISABLE_LOGGING_CONSOLE
+#define LOG(...) printf(__VA_ARGS__)
+#else
+#define LOG(...)
+#endif
---
+++ src/universalhookx/utils/utils.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+enum RenderingBackend_t {
+	NONE = 0,
+
+	DIRECTX9,
+	DIRECTX10,
+	DIRECTX11,
+	DIRECTX12,
+
+	OPENGL,
+	VULKAN,
+};
+
+namespace Utils {
+	void SetRenderingBackend(RenderingBackend_t eRenderingBackend);
+	RenderingBackend_t GetRenderingBackend( );
+	const char* RenderingBackendToStr( );
+
+	HWND GetProcessWindow( );
+	void UnloadDLL( );
+	
+	HMODULE GetCurrentImageBase( );
+
+	int GetCorrectDXGIFormat(int eCurrentFormat);
+
+	RenderingBackend_t GuessBackend();
+}
+
+namespace U = Utils;
